477bac3e376ddaaa3b6598f5548f02351da13d9f
Reinier de Vries <reiniervries@outlook.com>
2019-08-31 20:27:55 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EducatorGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exe = Environment.GetCommandLineArgs()[0]; // Command invocation part
+            var rawCmd = Environment.CommandLine;          // Complete command
+            var argsOnly = rawCmd.Remove(rawCmd.IndexOf(exe), exe.Length).TrimStart('"').Substring(1);
+            if (string.IsNullOrEmpty(argsOnly))
+            {
+                Console.WriteLine("Please provide an Educator curl command...");
+                Console.Read();
+                return;
+            }
+
+            // Remove the "curl " and " --compressed" parts of the command, because we're not using them.
+            var command = argsOnly
+                .RemovePreFix("curl ")
+                .Replace(" --compressed", string.Empty)
+                .EnsureStartsWith('\"');
+            // Make sure the the string starts with a quote and the first occurence of a space gets replaced by another quote.
+            var regex = new Regex(Regex.Escape(" "));
+            command = regex.Replace(command, "\" ", 1);
+            command = $"-L {command}";
+
+            // Run the curl command.
+            var p = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"C:\Windows\System32\curl.exe",
+                    Arguments = command,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = Environment.SystemDirectory
+                }
+            };
+            p.Start();
+
+            StringBuilder sb = new StringBuilder();
+            while (!p.StandardOutput.EndOfStream)
+            {
+                sb.AppendLine(p.StandardOutput.ReadLine());
+            }
+            p.Dispose();
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(sb.ToString());
+
+            var averageGrade = doc.DocumentNode
+                .SelectNodes("//dl[@class=\"exam-unit exam-unit__grade exam-unit-combined__grade\"]")
+                .Select(dl => dl.ChildNodes[1].ChildNodes[1].Attributes["data-content"].Value)
+                .Where(c => !c.Contains("Voldaan"))
+                .Average(v =>
+                {
+                    if (float.TryParse(v, NumberStyles.Float, CultureInfo.InvariantCulture, out float result))
+                    {
+                        return result;
+                    }
+
+                    return float.NaN;
+                });
+
+            Console.WriteLine(averageGrade);
+            Console.Read();
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducatorGrades
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Removes first occurrence of the given prefixes from beginning of the given string.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="preFixes">one or more prefix.</param>
+        /// <returns>Modified string or the same string if it has not any of given prefixes</returns>
+        public static string RemovePreFix(this string str, params string[] preFixes)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+
+            if (str == string.Empty)
+            {
+                return string.Empty;
+            }
+
+            if (preFixes.IsNullOrEmpty())
+            {
+                return str;
+            }
+
+            foreach (var preFix in preFixes)
+            {
+                if (str.StartsWith(preFix))
+                {
+                    return str.Right(str.Length - preFix.Length);
+                }
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Gets a substring of a string from end of the string.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="str"/> is null</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="len"/> is bigger that string's length</exception>
+        public static string Right(this string str, int len)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+            }
+
+            if (str.Length < len)
+            {
+                throw new ArgumentException("len argument can not be bigger than given string's length!");
+            }
+
+            return str.Substring(str.Length - len, len);
+        }
+
+        /// <summary>
+        /// Adds a char to beginning of given string if it does not starts with the char.
+        /// </summary>
+        public static string EnsureStartsWith(this string str, char c)
+        {
+            return EnsureStartsWith(str, c, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Adds a char to beginning of given string if it does not starts with the char.
+        /// </summary>
+        public static string EnsureStartsWith(this string str, char c, StringComparison comparisonType)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.StartsWith(c.ToString(), comparisonType))
+            {
+                return str;
+            }
+
+            return c + str;
+        }
+    }
+}
